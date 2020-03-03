b0c63d611ecf87f4cd96fa0cc354057bf19d6f29
Jack <gudtjrwns@gmail.com>
2020-03-03 16:06:19 +0900

    5 Commit [03 March 20]
    - application.properties 인코딩 설정

--- src/main/java/com/example/jacktest/controller/RestNoticeController.java
+++ src/main/java/com/example/jacktest/controller/RestNoticeController.java
@@ -1,9 +1,15 @@
 package com.example.jacktest.controller;
 
 import com.example.jacktest.dao.NoticeValue;
+import com.example.jacktest.entities.Notice;
 import com.example.jacktest.services.NoticeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.http.ResponseEntity;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -35,6 +41,39 @@ public class RestNoticeController {
         return noticeValueOne;
     }
 
+
+    // 게시판 - 목록
+//    @RequestMapping(value = "/list", method = RequestMethod.GET)
+//    public ResponseEntity<Page<Notice>> showNoticeList(ModelMap modelMap,
+//                                                       @PageableDefault(size = 10, page = 0) Pageable pageable) {
+//
+//        Page<Notice> noticeList = noticeService.pageAllNotice(pageable);
+//
+//        if (noticeList.isEmpty()) {
+//
+//
+//        } else {
+//        }
+//
+//
+//    }
+
+    // 게시판 - 검색 목록
+
+
+    // 게시판 - 신규 등록
+
+
+    // 게시판 - 수정
+
+
+    // 게시판 - 삭제
+
+
+    // 게시판 - 여러 항목 삭제
+
+
+    // 게시판 - 파일 다운로드
 
 
 }
