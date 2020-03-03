e29e9974a51e4546a59da77ec291406579d97f2b
Jack <gudtjrwns@gmail.com>
2020-03-03 15:30:03 +0900

    5 Commit [03 March 20]
    - test

--- src/main/java/com/example/jacktest/controller/RestNoticeController.java
+++ src/main/java/com/example/jacktest/controller/RestNoticeController.java
@@ -1,15 +1,9 @@
 package com.example.jacktest.controller;
 
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.io.UnsupportedEncodingException;
-
 import com.example.jacktest.dao.NoticeValue;
 import com.example.jacktest.services.NoticeService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -19,7 +13,7 @@ public class RestNoticeController {
     @Autowired
     private NoticeService noticeService;
 
-    
+
     // 게시글 제목 중복 확인
     @GetMapping("/findByTitleEquals/title={title}")
     public boolean findByTitleEquals(@PathVariable("title") String title) {
@@ -32,15 +26,15 @@ public class RestNoticeController {
     // 게시글 정보 가져오기
     @PostMapping("/findByNoticeOne/id={id}")
     public NoticeValue findByNoticeOne(@PathVariable("id") Long id) {
-        
+
         // 조회수 추가
         noticeService.addViewCount(id);
 
         NoticeValue noticeValueOne = noticeService.getNoticeValue(id);
-        
+
         return noticeValueOne;
     }
 
-    
-    
+
+
 }
